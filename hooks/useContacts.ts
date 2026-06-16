d205cb16ac5e1880e458755cc2d0d43447fe0322
Krzysztof Płonka <info@krzysztofplonka.pl>
2026-06-16 15:26:04 +0200

    import supabase from /lib/supabase in useContacts.ts

--- hooks/useContacts.ts
+++ hooks/useContacts.ts
@@ -1,14 +1,7 @@
-// lib/supabase.ts
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
-
 
 
 // hooks/useContacts.ts
+// 
 import { supabase } from "../lib/supabase";
 
 export async function getContacts() {
