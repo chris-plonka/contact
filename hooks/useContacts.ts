f31f99a729a7a20d6b9ce9d6bf9f55f0bcb38ce6
Krzysztof Płonka <info@krzysztofplonka.pl>
2026-06-16 15:17:02 +0200

    useContact add error

--- hooks/useContacts.ts
+++ hooks/useContacts.ts
@@ -1,3 +1,9 @@
-const { data } = await supabase
+const { data, error } = await supabase
   .from("contacts")
   .select("*");
+
+if (error) {
+  console.error(error);
+} else {
+  console.log(data);
+}
