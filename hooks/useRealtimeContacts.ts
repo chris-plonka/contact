24a5ecf30f4a0234442fd2d5837eb5a182979a20
Krzysztof Płonka <info@krzysztofplonka.pl>
2026-06-16 15:33:36 +0200

    useRealtimeContacts OK

--- hooks/useRealtimeContacts.ts
+++ hooks/useRealtimeContacts.ts
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import { supabase } from "../lib/supabase";
 
+export function useRealtimeContacts() {
+  useEffect(() => {
+    const channel = supabase
+      .channel("contacts")
+      .on(
+        "postgres_changes",
+        {
+          event: "*",
+          schema: "public",
+          table: "contacts",
+        },
+        (payload) => {
+          console.log(payload);
+        }
+      )
+      .subscribe();
 
-supabase
-  .channel("contacts")
-  .on("postgres_changes", ...)
+    return () => {
+      supabase.removeChannel(channel);
+    };
+  }, []);
+}
+
